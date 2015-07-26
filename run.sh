31db2ee014dec3f612209ba32030fec72bbdb466
Christos Nianiakas <cnianiak@gmail.com>
2015-07-26 22:25:53 +0300

    Updated run.sh. Using full path when invoking test.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ fi
 
 # Run embedded views tests
 cd seleniumTests/
-docker run -e 'CODEBENDER_SELENIUM_HUB_URL=$1' -it codebender/selenium /bin/bash test.sh
+docker run -i -t -e "CODEBENDER_SELENIUM_HUB_URL=$1" -it codebender/selenium /bin/bash /opt/codebender/seleniumTests/test.sh
 cd ..
